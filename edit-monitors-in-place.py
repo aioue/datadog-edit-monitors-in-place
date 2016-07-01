d99c4f698a1b20e2d46e5468b94126f9ed19f1ea
Tom Paine <aioue@users.noreply.github.com>
2016-07-01 15:48:17 +0100

    Update edit-monitors-in-place.py

--- edit-monitors-in-place.py
+++ edit-monitors-in-place.py
@@ -1,11 +1,12 @@
-# This script will download all monitors to a file and allow them to be edited in place before uploading them again.
+#! /usr/bin/env python
+#
+# Download existing monitors to a file and allow them to be edited in place, before uploading them again.
 # Any changes will be reflected in the DD control panel as updates to existing monitors.
-# Does not delete existing monitors.
 
 import json
 from time import gmtime, strftime
+
 from datadog import initialize
-import yaml
 
 options = {
     'api_key': 'foo',
@@ -19,14 +20,17 @@ from datadog import api
 
 datadog_monitors = api.Monitor.get_all()
 
-# print(json.dumps(datadog_monitors, indent=2, sort_keys=True))
+timestamp = strftime("%Y-%m-%d_%H:%M:%S", gmtime())
 
+# print(json.dumps(datadog_monitors, indent=2, sort_keys=False))
+
+print timestamp
 print "Number of monitors: " + str(len(datadog_monitors))
 
 with open('existing_monitors.json', 'w') as outfile:
-    json.dump(datadog_monitors, outfile, indent=2, sort_keys=True)
+    json.dump(datadog_monitors, outfile, indent=2, sort_keys=False)
 
-wait = raw_input("Edit 'existing_monitors.json', then press a key to upload the changes.")
+wait = raw_input("Edit 'existing_monitors.json', save, then press a key to upload the changes.")
 
 with open('existing_monitors.json', 'r') as infile:
     imported_datadog_monitors = json.load(infile)
